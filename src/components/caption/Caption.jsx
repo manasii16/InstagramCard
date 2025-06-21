120b4e9a6644b1b1933e5148dd494fa9e1a20d29
Manasi <manasishivarkar@gmail.com>
2025-06-21 15:51:09 +0530

    add log to caption component

--- src/components/caption/Caption.jsx
+++ src/components/caption/Caption.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import styles from './Caption.module.css';
 
 export default function Caption(){
+  console.log('caption component');
   return (
     <div className={styles.caption}>
       <b>manasii.shivarkar</b> Heyy!
